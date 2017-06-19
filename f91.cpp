7115c82da8e3057db5a01472a1d2f5ceadbc4f03
Ruhul amin <ruhul.cse7862@gmail.com>
2017-06-19 23:24:31 +0600

    UVA 10696 solve

---
+++ f91.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+        int a,b;
+        while(scanf("%d",&a)==1 && a!=0)
+        {if(a>100)
+        b=a-10;
+        else
+        b=91;
+        printf("f91(%d) = %d\n",a,b);}
+        return 0;
+}
