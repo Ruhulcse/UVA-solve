c6f9a045ebe91df3780e645a707a0ec2436fc4b1
Ruhul amin <ruhul.cse7862@gmail.com>
2017-09-30 01:19:12 +0600

    UVA 12952 Solution

---
+++ Tri-du.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+
+   while(scanf("%d %d",&a,&b)==2)
+   {
+
+        if(a>b)
+        {
+            cout<<a<<endl;
+        }
+        else
+        {
+            cout<<b<<endl;
+        }
+
+   }
+   return 0;
+}
