3d2c0c15c60cfab53a02a3b348ad7522b42f185b
Ruhul amin <ruhul.cse7862@gmail.com>
2018-11-26 12:31:41 +0600

    Eulers Totient Function /Number theory

---
+++ spoj/Euler Totient Function.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int phi(int n)
+{
+    int result=n;
+    for(int p=2;p*p<=n;++p)
+    {
+        if(n%p==0)
+        {
+            while(n%p==0)
+                n/=p;
+            result-=result/p;
+        }
+    }
+    if(n>1)
+        result-=result/n;
+    return result;
+}
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        int n;
+        cin>>n;
+        cout<<phi(n)<<endl;
+    }
+    return 0;
+}
+ 
